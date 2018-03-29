4fd867cd78a95be727e99bf725f4ec8e005ec5d3
TienLS <neotien200065@gmail.com>
2018-03-29 16:56:16 +0700

    first commit + Project 1: Hello Angular

---
+++ tls-first-app/src/tls-first-app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tls-app',
+  templateUrl: '../template/app.template.html',
+  styles: [``],
+  styleUrls: ['../template/app.styles.css']
+  
+})
+export class AppComponent {
+  title = 'Tls First App';
+}
