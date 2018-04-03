6f978300b0d6522dd1672083435d42322b837f04
TienLS <neotien200065@gmail.com>
2018-04-03 13:37:04 +0700

    Chương 2:: 2.5 - 2.7: Setting Template:: ChangeColor, ChangeContent.

--- tls-first-app/src/project-three/setting-template/setting-template.component.ts
+++ tls-first-app/src/project-three/setting-template/setting-template.component.ts
@@ -7,5 +7,36 @@ import { Component } from '@angular/core';
 })
 
 export class SettingTemplateComponent {
-    
+    txtContent      : string = "";
+    txtColor        : string = "";
+    bgColor         : string = "";
+    currentTemplate : string = "";
+
+
+    constructor(){
+        this.txtContent = "Angular 4x is not fun :(";
+        this.txtColor   = "#ffffff";
+        this.bgColor    = "#000000";
+        this.currentTemplate = "optBlue";
+    }
+
+    changeTemplate($event){
+        this.currentTemplate = $event.target.value;
+        console.log(this.currentTemplate);
+    }
+
+    changeBgColor($event){
+        this.bgColor = $event.target.value;
+    }
+
+    changeTxtColor($event){
+        this.txtColor = $event.target.value;
+    }
+
+    setCurrentStyles() {
+        return {
+            'background-color' :  this.bgColor,
+            'color'            :  this.txtColor,
+        };
+    }
 }
